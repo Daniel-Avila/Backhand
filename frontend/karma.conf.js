5bc66ceddae47ab8452b90ff64130fed080a63c3
Daniel Avila (sparky) <sparkytwobillion@gmail.com>
2015-07-26 11:55:38 -0700

    Moved in Unittest framework.

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -9,9 +9,14 @@ module.exports = function (config) {
             'app/bower_components/angular-mocks/angular-mocks.js',
             'app/bower_components/angular-resource/angular-resource.js',
             'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+            'app/bower_components/angular-cookies/angular-cookies.js',
+            'app/bower_components/angular-sanitize/angular-sanitize.js',
+            'app/bower_components/angular-route/angular-route.js',
             'app/components/**/*.js',
+            'app/components/scripts/**/*.js',
+            'app/components/scripts/*.js',
             'tests/**/*.js',
-            'tests/*.js'
+            'tests/*.js',
         ],
 
         autoWatch: false,
