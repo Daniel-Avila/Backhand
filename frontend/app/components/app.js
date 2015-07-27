204e362daf0861254703db266b6b00a0ad482201
Daniel Avila (sparky) <sparkytwobillion@gmail.com>
2015-07-26 18:14:43 -0700

    Starting to look pertty

--- frontend/app/components/app.js
+++ frontend/app/components/app.js
@@ -2,23 +2,8 @@
 
 // Declare app level module which depends on views, and components
 angular.module('Backhand', ['angularDjangoRegistrationAuthApp'])
-    .constant('cst', {
-        APP: {
-            LOGIN: '/login/',
-            DASHBOARD: '/dashboard/',
-            HOME: '/'
-        },
-        REST: {
-            LOGIN: '/api/v1/auth/login/'
-        },
-        TEMPLATES: {
-            HOME: 'templates/home.html',
-            LOGIN: 'templates/login.html',
-            DASHBOARD: 'templates/dashboard.html'
-        },
-        STATE: {
-            HOME: 'home',
-            LOGIN: 'login',
-            DASHBOARD: 'dashboard'
-        }
-    });
+    .config(['$stateProvider', '$urlRouterProvider',
+        function ($stateProvider) {
+            $stateProvider
+
+        }]);
