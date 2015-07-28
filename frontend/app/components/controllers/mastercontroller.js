52cdc5296083c93480e9480ce032d58dba779e79
Daniel Avila (sparky) <sparkytwobillion@gmail.com>
2015-07-28 08:04:01 -0700

    overrode $scope.$on for login event in new master controller.

---
+++ frontend/app/components/controllers/mastercontroller.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * Created by sparky on 7/28/15.
+ */
+'use strict';
+
+angular.module('Backhand')
+    .controller('MasterController', function ($scope, $state, $controller) {
+        $controller('MasterCtrl', {$scope: $scope});
+        $scope.$on('djangoAuth.logged_in', function (data) {
+            $scope.authenticated = true;
+            $state.go('dashboard');
+        })
+    });
