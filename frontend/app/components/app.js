0ab0e28489db58bbe13a57bb261ebc5b979c4b3b
Daniel Avila (sparky) <sparkytwobillion@gmail.com>
2015-07-30 14:06:00 -0700

    Preferences page with accordian nav now working as a shell.

--- frontend/app/components/app.js
+++ frontend/app/components/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('Backhand', ['angularDjangoRegistrationAuthApp'])
+angular.module('Backhand', ['ui.bootstrap', 'angularDjangoRegistrationAuthApp'])
     .config(['$stateProvider', '$urlRouterProvider',
         function ($stateProvider) {
             $stateProvider
--- frontend/app/components/controllers/preferencescontroller.js
+++ frontend/app/components/controllers/preferencescontroller.js
@@ -1,5 +1,6 @@
 "use strict";
 
 angular.module('Backhand')
-    .controller('PreferencesController', function () {
+    .controller('PreferencesController', function ($scope) {
+        $scope.currentTemplate = {path: 'views/userprofile.html'}
     });
